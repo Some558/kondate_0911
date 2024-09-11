285323e39a631fd4829be25e907d8245618b203f
Some558 <167545573+Some558@users.noreply.github.com>
2024-09-11 22:25:13 +0900

    献立候補関係ファイルall作成

---
+++ kondate_portfolio/app/Http/Controllers/MenuOptionsController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreMenu_OptionsRequest;
+use App\Http\Requests\UpdateMenu_OptionsRequest;
+use App\Models\Menu_Options;
+
+class MenuOptionsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreMenu_OptionsRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Menu_Options $menu_Options)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Menu_Options $menu_Options)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateMenu_OptionsRequest $request, Menu_Options $menu_Options)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Menu_Options $menu_Options)
+    {
+        //
+    }
+}

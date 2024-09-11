c09327a56a07d1caba838c1b53e03de27e84747d
Some558 <167545573+Some558@users.noreply.github.com>
2024-09-11 17:11:44 +0900

    ログインすると、admin.topに遷移する

--- kondate_portfolio/app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ kondate_portfolio/app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -27,8 +27,8 @@ class AuthenticatedSessionController extends Controller
         $request->authenticate();
 
         $request->session()->regenerate();
-
-        return redirect()->intended(route('dashboard', absolute: false));
+        //管理画面トップページにリダイレクト
+        return redirect()->intended(route('admin.top', absolute: false));
     }
 
     /**

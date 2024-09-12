b2d7506a982684a2403c82c4c4006f16d65c0013
Some558 <167545573+Some558@users.noreply.github.com>
2024-09-12 22:49:30 +0900

    migrationファイル修正後、migrate

--- kondate_portfolio/database/migrations/2024_09_11_131646_create_menu__options_table.php
+++ kondate_portfolio/database/migrations/2024_09_11_131646_create_menu__options_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('menu__options', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('dish_name')->max(20)->comment('献立名');
+            $table->enum('dish_type', ['main', 'sub1', 'sub2'])->comment('献立タイプ');
             $table->timestamps();
         });
     }

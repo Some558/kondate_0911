1fd051c501ee1a44ff3a883b87933490bd3cf01c
Some558 <167545573+Some558@users.noreply.github.com>
2024-09-13 22:52:34 +0900

    admintopに献立候補新規登録ボタン追加

--- kondate_portfolio/resources/views/admin/top.blade.php
+++ kondate_portfolio/resources/views/admin/top.blade.php
@@ -1,3 +1,15 @@
 <x-admin-layout>
-<h1>管理画面トップページです</h1>
+    <section class="text-gray-600 body-font relative">
+        <div class="container px-5 py-24 mx-auto">
+            <div class="flex flex-col text-center w-full mb-12">
+                <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">献立候補を登録して下さい</h1>
+                           </div>
+                    <div class="p-2 w-full">
+                        <button class="flex mx-auto text-white bg-yellow-500 border-0 py-2 px-8 focus:outline-none hover:bg-yellow-600 rounded text-lg">献立候補　新規登録</button>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </section>
 </x-admin-layout>

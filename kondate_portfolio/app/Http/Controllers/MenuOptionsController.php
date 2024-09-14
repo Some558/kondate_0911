3cc0ce700456b5917374e1c0ef6fbde715518804
Some558 <1641043@gmail.com>
2024-09-14 22:45:06 +0900

    献立候補をまとめて登録できるように変更

--- kondate_portfolio/app/Http/Controllers/MenuOptionsController.php
+++ kondate_portfolio/app/Http/Controllers/MenuOptionsController.php
@@ -24,20 +24,38 @@ class MenuOptionsController extends Controller
         return view('admin.menu_options.create');
     }
 
+    // public function store(StoreMenu_OptionsRequest $request)
+    // {
+    //     // dd('カテゴリー新規登録処理のルートです', $request);
+    //     // dd($request->name, $request->type);
+    //     $menu_options = new Menu_Options();
+    //     $menu_options->dish_name = $request->name;
+    //     $menu_options->dish_type = $request->type;
+    //     $menu_options->user_id = auth()->id();
+    //     $menu_options->save();
+
     /**
      * 献立新規登録処理
      */
     public function store(StoreMenu_OptionsRequest $request)
     {
-        // dd('カテゴリー新規登録処理のルートです', $request);
-        // dd($request->name, $request->type);
-        $menu_options = new Menu_Options();
-        $menu_options->dish_name = $request->name;
-        $menu_options->dish_type = $request->type;
-        $menu_options->user_id = auth()->id();
-        $menu_options->save();
+        $names = $request->input('names');
+        $types = $request->input('types');
 
-        return redirect()->route('admin.top');
+        foreach ($names as $index => $name) {
+            // 名前とタイプが空でないことを確認
+            if (!empty($name) && !empty($types[$index])) {
+                $menu_options = new Menu_Options();
+                $menu_options->dish_name = $name;
+                $menu_options->dish_type = $types[$index];
+                $menu_options->user_id = auth()->id(); // ユーザーIDを設定
+                $menu_options->save();
+            }
+        }
+        // 保存成功後のメッセージをセッションに格納
+        session()->flash('success', '献立候補の新規登録が完了しました。');
+        // 保存成功後のメッセージをセッションに格納
+        return redirect()->route('admin.top')->with('success', '献立候補の新規登録が完了しました。');
     }
 
     /**
--- kondate_portfolio/resources/views/admin/top.blade.php
+++ kondate_portfolio/resources/views/admin/top.blade.php
@@ -1,4 +1,9 @@
 <x-admin-layout>
+    @if (session('success'))
+        <div class="alert alert-success text-green-500">
+            {{ session('success') }}
+        </div>
+    @endif
     <section class="text-gray-600 body-font relative">
         <div class="container px-5 py-24 mx-auto">
             <div class="flex flex-col text-center w-full mb-12">
